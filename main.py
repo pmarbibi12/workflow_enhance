e5b0c5e9ad2d35a07be269e0902cecfc8e95ab03
pmarbibi12 <marbibipanfilo@gmail.com>
2023-09-21 00:41:59 -0500

    frames configured

--- main.py
+++ main.py
@@ -19,61 +19,99 @@ class App(customtkinter.CTk):
 
         # configure window
         self.title("Workflow Status Getter")
-        self.geometry(f"{1100}x{580}")
+        self.geometry(f"{1200}x{700}")
 
         # configure grid layout (4x4)
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_columnconfigure((0, 1), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
+
+        ## sidebar frame
 
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=10)
         self.sidebar_frame.grid(row=0, column=0, rowspan=10, padx = 10, pady = 10, sticky="nsew")
 
-        #row 0
+        # row 0
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Load Workflow", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
 
-        #row 1,2
+        # row 1,2
         self.workflow_id_label = customtkinter.CTkLabel(self.sidebar_frame, text="Workflow ID:", anchor="w")
         self.workflow_id_label.grid(row=1, column=0, padx=20, pady=(10, 0))
         self.workflow_id = customtkinter.CTkEntry(self.sidebar_frame, placeholder_text="Workflow ID")
         self.workflow_id.grid(row=2, column=0, padx=20, pady=10)
 
-        #row 3,4
+        # row 3,4
         self.workflow_dataowner_label = customtkinter.CTkLabel(self.sidebar_frame, text="DataOwner ID:", anchor="w")
         self.workflow_dataowner_label.grid(row=3, column=0, padx=20, pady=(10, 0))
         self.workflow_dataowner = customtkinter.CTkEntry(self.sidebar_frame, placeholder_text="Environment Specific")
         self.workflow_dataowner.grid(row=4, column=0, padx=20, pady=10)
 
-        #row 5,6
+        # row 5,6
         self.environment_selector_label = customtkinter.CTkLabel(self.sidebar_frame, text="Environment:", anchor="w")
         self.environment_selector_label.grid(row=5, column=0, padx=20, pady=(10, 0))
         self.environment_selector = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Prod", "UAT", "QA"])
         self.environment_selector.grid(row=6, column=0, padx=20, pady=(10, 10))
 
-        #row 7,8
+        # row 7,8
         self.auth_entry_label = customtkinter.CTkLabel(self.sidebar_frame, text="Auth Code:", anchor="w")
         self.auth_entry_label.grid(row=7, column=0, padx=20, pady=(10, 0))
         self.auth_entry = customtkinter.CTkEntry(self.sidebar_frame, placeholder_text="Auth Code")
         self.auth_entry.grid(row=8, column=0, padx=20, pady=10)
 
-        #row 9
+        # row 9
         self.load_button = customtkinter.CTkButton(self.sidebar_frame, text="Load", command=self.load_pressed)
         self.load_button.grid(row=9, column=0, padx=20, pady=10)
 
-        ##product GUIDs Frame
+        ### search Frame
 
-        self.product_guids_frame = customtkinter.CTkFrame(self, width=280, corner_radius=10)
-        self.product_guids_frame.grid(row=0, column=1, rowspan=10, padx = (0,10), pady = 10, sticky="nsew")
+        self.search_frame = customtkinter.CTkFrame(self, width=280, corner_radius=10)
+        self.search_frame.grid(row=0, column=1, rowspan=10, padx = (0,10), pady = 10, sticky="nsew")
+        self.search_frame.grid_rowconfigure(1, weight=1)
+        self.search_frame.grid_rowconfigure(0, weight=0)
+        self.search_frame.grid_columnconfigure(0, weight=1)
+        
+        ## search rows
 
-        self.product_guids_label = customtkinter.CTkLabel(self.product_guids_frame, text="Product Guids:", anchor="w")
-        self.product_guids_label.grid(row=0, column=0, padx=20, pady=(10, 0))
+        # row 0
+        self.product_guids_label = customtkinter.CTkLabel(self.search_frame, text="Product GUIDs:", justify= "left")
+        self.product_guids_label.grid(row=0, column=0, padx=20, pady=10)
+        
+        # row 1
+        ### search-productguids frame 
+        self.product_guids_frame = customtkinter.CTkFrame(self.search_frame, corner_radius=10, fg_color="transparent")
+        self.product_guids_frame.grid(row=1, column=0, rowspan=10, padx = 10, sticky="nsew")
+        self.product_guids_frame.grid_columnconfigure(0, weight=1)
+        self.product_guids_frame.grid_rowconfigure(0, weight=1)
+        self.product_guids_frame.grid_rowconfigure(1, weight=0)
 
-        self.product_guids_textbox = customtkinter.CTkTextbox(self.product_guids_frame, width=270,)
-        self.product_guids_textbox.grid(row=1, column=0, padx=(20, 0), pady=(20, 0), sticky="nsew")
 
+        ## search-productguids rows
+
+        # row 0
+        self.product_guids_textbox = customtkinter.CTkTextbox(self.product_guids_frame)
+        self.product_guids_textbox.grid(row=0, column=0, sticky="nsew")
+        
+        # row 1
         self.search_guids_button = customtkinter.CTkButton(self.product_guids_frame, text="Search", command=self.load_pressed)
-        self.search_guids_button.grid(row=2, column=0, padx=20, pady=(10, 0))
+        self.search_guids_button.grid(row=1, column=0, padx=20, pady=15)
+
+        ## output Frame
+        self.output_frame = customtkinter.CTkFrame(self, width=500, corner_radius=10)
+        self.output_frame.grid(row=0, column=2, rowspan=10, padx = (0,10), pady = 10, sticky="nsew")
+        self.output_frame.grid_columnconfigure(0, weight = 1)
+        self.output_frame.grid_rowconfigure(1, weight = 1)
+
+
+        # ouput frame rows
+
+        self.output_label = customtkinter.CTkLabel(self.output_frame, text="Output:", justify= "left")
+        self.output_label.grid(row=0, column=0, padx=20, pady=(10,0))
+
+        self.df_frame = customtkinter.CTkFrame(self.output_frame)
+        self.df_frame.grid(row=1, column=0, rowspan=10, padx =15, pady = (10,15), sticky="nsew")
+
+
 
 
     def load_pressed(self):
